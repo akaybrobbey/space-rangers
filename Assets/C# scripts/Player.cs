d54e76c7ebffca0137059a9a72e180f1a081b1df
akaybrobbey <akaybrobbey@gmail.com>
2022-02-28 14:12:49 +0000

    feat: init

---
+++ Assets/C# scripts/Player.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int keys = 0;
+    public float speed = 5.0f;
+    
+
+    public Text keyAmount;
+    public Text youWin;
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //code to move
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+        //code to destroy door
+
+        if(keys==3)
+        {
+            Destroy(door);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        //to collect keys
+        if (collision.gameObject.tag == "Keys")
+        {
+            keys++;
+            keyAmount.text = "Keys: " + keys;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Princess")
+        {
+            youWin.text = "You Win!";
+        }
+        //to reset gamestate on collision with enemies
+        if (collision.gameObject.tag == "Enemies")
+
+            //where the lives code will be
+        
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        //to bounce off walls
+        if (collision.gameObject.tag == "Walls")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
